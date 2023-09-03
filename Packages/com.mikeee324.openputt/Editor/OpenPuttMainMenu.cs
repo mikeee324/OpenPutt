5d680ac7b367b8be2ceeb166af10845ffda35768
mikeee324 <mikeee324@gmail.com>
2023-09-03 17:15:00 +0100

    Fixes new editor window causing builds to fail

--- Packages/com.mikeee324.openputt/Editor/OpenPuttMainMenu.cs
+++ Packages/com.mikeee324.openputt/Editor/OpenPuttMainMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -176,3 +177,4 @@ namespace mikeee324.OpenPutt
         }
     }
 }
+#endif
