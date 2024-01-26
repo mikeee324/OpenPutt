a746b98d4aa813ced44120d2cd948f4385eac9b3
mikeee324 <mikeee324@gmail.com>
2024-01-26 20:15:02 +0000

    Adds DragChangeArea script to allow changing drag while the ball is inside the collider

---
+++ Packages/com.mikeee324.openputt/Runtime/Scripts/GolfBallDragChangeArea.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace mikeee324.OpenPutt
+{
+
+    /// <summary>
+    /// Listens for a golf ball entering/exiting a collider on this game object.<br/>
+    /// It will apply or remove the dragInArea value to the golf ball to effect how quickly the ball loses its speed while in this collider
+    /// </summary>
+    public class GolfBallDragChangeArea : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private float dragInArea = 0f;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            GolfBallController golfBall = other.GetComponent<GolfBallController>();
+            if (golfBall == null)
+                return;
+
+            golfBall.ballDragOverride = dragInArea;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            GolfBallController golfBall = other.GetComponent<GolfBallController>();
+            if (golfBall == null)
+                return;
+
+            golfBall.ballDragOverride = 0f;
+        }
+
+    }
+
+}
