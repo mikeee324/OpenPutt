ead5e69ba01dce01507e6d33de3cba92df8b92b1
mikeee324 <mikeee324@gmail.com>
2024-01-27 15:38:13 +0000

    Sets sync mode on GolfBallDragChangeArea

--- Packages/com.mikeee324.openputt/Runtime/Scripts/GolfBallDragChangeArea.cs
+++ Packages/com.mikeee324.openputt/Runtime/Scripts/GolfBallDragChangeArea.cs
@@ -11,6 +11,7 @@ namespace mikeee324.OpenPutt
     /// Listens for a golf ball entering/exiting a collider on this game object.<br/>
     /// It will apply or remove the dragInArea value to the golf ball to effect how quickly the ball loses its speed while in this collider
     /// </summary>
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None), DefaultExecutionOrder(0)]
     public class GolfBallDragChangeArea : UdonSharpBehaviour
     {
         [SerializeField]
