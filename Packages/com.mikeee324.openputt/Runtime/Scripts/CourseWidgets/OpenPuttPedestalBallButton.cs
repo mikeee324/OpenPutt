305618ca486973c1cb18e664942b0ccda7758d57
mikeee324 <mikeee324@gmail.com>
2026-07-16 22:26:55 +0100

    Adds in a couple of new scripts that will spawn in your golf club and ball by clicking a button. Made for people who struggle with the shoulder pickups.

---
+++ Packages/com.mikeee324.openputt/Runtime/Scripts/CourseWidgets/OpenPuttPedestalBallButton.cs
@@ -0,0 +1,41 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace dev.mikeee324.OpenPutt
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class OpenPuttPedestalBallButton : UdonSharpBehaviour
+    {
+        public OpenPutt openPutt;
+
+        [Tooltip("Where the local player's ball will be moved to when this button is interacted with")]
+        public Transform targetPosition;
+
+        [Tooltip("If enabled the ball will have BallIsMoving set to true after being moved so it can drop/roll instead of staying frozen in place")]
+        public bool enableBallMovingAfterMove = false;
+
+        public override void Interact()
+        {
+            if (!Utilities.IsValid(openPutt) || !Utilities.IsValid(openPutt.LocalPlayerManager) || !Utilities.IsValid(openPutt.LocalPlayerManager.golfBall) || !Utilities.IsValid(targetPosition))
+                return;
+
+            var playerManager = openPutt.LocalPlayerManager;
+            var golfBall = playerManager.golfBall;
+
+            // Make sure the ball is active/visible before touching its physics, otherwise the
+            // changes below won't take effect properly on a disabled GameObject
+            playerManager.BallVisible = true;
+
+            golfBall.BallIsMoving = false;
+
+            golfBall._SetPosition(targetPosition.position);
+            golfBall._SetRespawnPosition(targetPosition.position);
+
+            if (enableBallMovingAfterMove)
+                golfBall.BallIsMoving = true;
+
+            playerManager._RequestSync(syncNow: true);
+        }
+    }
+}
---
+++ Packages/com.mikeee324.openputt/Runtime/Scripts/CourseWidgets/OpenPuttPedestalClubButton.cs
@@ -0,0 +1,49 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace dev.mikeee324.OpenPutt
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class OpenPuttPedestalClubButton : UdonSharpBehaviour
+    {
+        public OpenPutt openPutt;
+
+        [Tooltip("Where the local player's club will be moved to when this button is interacted with")]
+        public Transform targetPosition;
+
+        [Tooltip("If enabled the club will have its physics re-enabled after being moved so it can drop instead of staying frozen in place")]
+        public bool enableClubPhysicsAfterMove = false;
+
+        public override void Interact()
+        {
+            if (!Utilities.IsValid(openPutt) || !Utilities.IsValid(openPutt.LocalPlayerManager) || !Utilities.IsValid(openPutt.LocalPlayerManager.golfClub) || !Utilities.IsValid(targetPosition))
+                return;
+
+            var playerManager = openPutt.LocalPlayerManager;
+            var golfClub = playerManager.golfClub;
+
+            // Can't move the club while it's attached to a hand, so drop it first
+            if (golfClub.CurrentHand != VRC_Pickup.PickupHand.None)
+                golfClub.pickup.Drop();
+
+            // Make sure the club is active/visible before touching its physics, otherwise the
+            // changes below won't take effect properly on a disabled GameObject
+            playerManager.ClubVisible = true;
+
+            golfClub.clubRigidbody.isKinematic = true;
+            golfClub.clubRigidbody.position = targetPosition.position;
+            golfClub.clubRigidbody.rotation = targetPosition.rotation;
+
+            if (enableClubPhysicsAfterMove)
+            {
+                golfClub.clubRigidbody.isKinematic = false;
+                golfClub.clubRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+                golfClub.handleCollider.isTrigger = false;
+                golfClub.shaftCollider.isTrigger = false;
+            }
+
+            playerManager._RequestSync(syncNow: true);
+        }
+    }
+}
